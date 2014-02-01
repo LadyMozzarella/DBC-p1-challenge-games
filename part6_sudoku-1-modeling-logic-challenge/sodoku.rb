a9c7a18c79c7641a2b3a769a91d1df1661e8734e
Chris Woodrich & Jamie <pair+chriswoodrich.jamck23@devbootcamp.com>
2014-01-31 19:31:09 -0800

    Working box assignment method

--- part6_sudoku-1-modeling-logic-challenge/sodoku.rb
+++ part6_sudoku-1-modeling-logic-challenge/sodoku.rb
@@ -4,7 +4,7 @@ require 'set'
 digits = Set.new [1,2,3,4,5,6,7,8,9]
 
 class Tile
-  attr_accessor :value, :possibles, :row, :column
+  attr_accessor :value, :possibles, :row, :column, :box_number
   def initialize(value)
     @possibles = pop_set(value)
     @value = value
@@ -13,7 +13,10 @@ class Tile
   def pop_set(value)
     if value == "0"
       set = Set.new [1,2,3,4,5,6,7,8,9]
+    else
+      set = Set.new [value]
     end
+    return set
   end
 
   def eliminate_possibles(data)
@@ -43,12 +46,16 @@ class Board
       end
     end
   end
+  # def assign_boxes
+
+  # end
 
   def populate_tiles
     @board.each_with_index do |row, row_index|
       row.each_with_index do |this_tile, column_index|
         this_tile.row = row_index
         this_tile.column = column_index
+        p this_tile.box_number = (3 * (this_tile.row / 3)) + ((this_tile.column/3) +1)
       end
     end
   end
@@ -64,8 +71,28 @@ end
 
 
 
-OurBoard = Board.new()
-OurBoard.set_up("619030040270061008000047621486302079000014580031009060005720806320106057160400030")
-OurBoard.create_tiles
-OurBoard.display
-OurBoard.populate_tiles
+our_board = Board.new()
+our_board.set_up("619030040270061008000047621486302079000014580031009060005720806320106057160400030")
+our_board.create_tiles
+puts "Sudoku Board: "
+puts our_board.display
+our_board.populate_tiles
+
+# print our_board.board[2][4].row
+# print our_board.board[2][4].column
+
+row_values = []
+our_board.board[0].each do |tile|
+  row_values << tile.value.to_i if tile.value != "0"
+end
+puts "Going to subtract: #{row_values}"
+
+column_values = []
+our_board.board.each do |row|
+  column_values << row[3].value.to_i if row[3].value != "0"
+end
+puts "Going to subtract: #{column_values}"
+
+# puts our_board.board[2][3].possibles
+# our_board.board[2][3].eliminate_possibles(row_values)
+# our_board.board[2][3].eliminate_possibles(column_values)
